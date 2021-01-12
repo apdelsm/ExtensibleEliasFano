28926cab031bd520a00fc414940eaf03906d82dd
apdelsm <apdelsm@gmail.com>
2021-01-12 09:04:39 -0300

    main file

---
+++ src/main.cpp
@@ -0,0 +1,14 @@
+#include <vector>
+
+#include <sdsl/bit_vectors.hpp>
+
+#include "../include/pairsVector.hpp"
+#include "../include/linearSearch.hpp"
+#include "../include/extensibleEliasFano.hpp"
+
+using namespace std;
+using namespace sdsl;
+
+int main() {
+  return 0;
+}
