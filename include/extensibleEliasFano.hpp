ce0f4549974333870406899a85a23d1375108aec
apdelsm <apdelsm@gmail.com>
2021-01-18 02:44:32 -0300

    add destructor and select1 function

--- include/extensibleEliasFano.hpp
+++ include/extensibleEliasFano.hpp
@@ -15,15 +15,16 @@ class ExtensibleEliasFano {
     vector<uint64_t> buffer;
     uint64_t bufferFill = 0;
     uint64_t count = 0;
-    uint64_t bufferCount = 0;
+    uint64_t blocksCount = 0;
     T predecessorStructure;
     char whereIsPredecessor(uint64_t element);
     vector<tuple<uint64_t, uint64_t, sd_vector<>>*> blocks;
 
   public:
     ExtensibleEliasFano(uint64_t bufferSize, T predecessorStructure);
+    ~ExtensibleEliasFano();
     void pushBit(uint64_t bit);
-    uint64_t select1(uint64_t occurrence);
+    int select1(uint64_t occurrence, uint64_t &result);
 };
 
 template <class T>
@@ -31,6 +32,13 @@ ExtensibleEliasFano<T>::ExtensibleEliasFano(uint64_t bufferSize, T predecessorSt
   : predecessorStructure(predecessorStructure)
 {
   this->buffer = vector<uint64_t>(bufferSize, 0);
+}
+
+template <class T>
+ExtensibleEliasFano<T>::~ExtensibleEliasFano() {
+  for (vector<tuple<uint64_t, uint64_t, sd_vector<>>*>::iterator it = blocks.begin(), end = blocks.end(); it != end; ++it) {
+    delete *it;
+  }
 }
 
 template <class T>
@@ -44,10 +52,10 @@ void ExtensibleEliasFano<T>::pushBit(uint64_t bit) {
       for (vector<uint64_t>::iterator it = (this->buffer).begin(), end = (this->buffer).end(); it != end; ++it) {
         *it -= first;
       }
-      tuple<uint64_t, uint64_t, sd_vector<>> bufferSDVector = make_tuple(this->bufferCount, first, sd_vector<>((this->buffer).begin(), (this->buffer).end()));
-      blocks.push_back(&bufferSDVector);
+      tuple<uint64_t, uint64_t, sd_vector<>>* bufferSDVector = new tuple<uint64_t, uint64_t, sd_vector<>>(this->blocksCount, first, sd_vector<>((this->buffer).begin(), (this->buffer).end()));
+      blocks.push_back(bufferSDVector);
       this->predecessorStructure.push(bufferSDVector);
-      ++this->bufferCount;
+      ++this->blocksCount;
     }
   }
   count++;
@@ -69,7 +77,22 @@ char ExtensibleEliasFano<T>::whereIsPredecessor(uint64_t element) {
 }
 
 template <class T>
-uint64_t ExtensibleEliasFano<T>::select1(uint64_t occurrence) {
+int ExtensibleEliasFano<T>::select1(uint64_t occurrence, uint64_t &result) {
+  uint64_t block = (occurrence-1)/buffer.size();
+  if (block > blocksCount) {
+    return 0;
+  } else if (block < blocksCount) {
+    tuple<uint64_t, uint64_t, sd_vector<>>* containerBlock = blocks[block];
+    uint64_t subOccurrence = occurrence - get<0>(*containerBlock) * uint64_t(buffer.size());
+    sd_vector<>::select_1_type selectSD(&(get<2>(*containerBlock)));
+    result = selectSD(subOccurrence) + get<1>(*containerBlock);
+    return 1;
+  } else {
+    if (occurrence <= blocksCount*buffer.size() + bufferFill) {
+      result = buffer[occurrence - blocksCount*buffer.size() - 1];
+      return 1;
+    }
+  }
   return 0;
 }
 
