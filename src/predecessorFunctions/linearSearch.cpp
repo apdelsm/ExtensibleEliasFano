ef3b019faa1326ce06a57a90493ec35189a84b16
apdelsm <apdelsm@gmail.com>
2021-01-24 13:45:22 -0300

    remove unnecessary parentheses

--- src/predecessorFunctions/linearSearch.cpp
+++ src/predecessorFunctions/linearSearch.cpp
@@ -10,9 +10,9 @@ using namespace sdsl;
 #define LINEARSEARCH
 
 tuple<uint64_t, uint64_t, sd_vector<>>* linearSearch(vector<tuple<uint64_t, uint64_t, sd_vector<>>> &orderedVector, uint64_t element) {
-  for(vector<tuple<uint64_t, uint64_t, sd_vector<>>>::iterator it = (orderedVector).begin(), end = (orderedVector).end(); it != end; ++it) {
+  for(vector<tuple<uint64_t, uint64_t, sd_vector<>>>::iterator it = orderedVector.begin(), end = orderedVector.end(); it != end; ++it) {
     if(element < get<1>(*it)) {
-      if (get<1>(*it) == get<1>(*(orderedVector).begin())) {
+      if (get<1>(*it) == get<1>(*orderedVector.begin())) {
         return NULL;
       }
       --it;
