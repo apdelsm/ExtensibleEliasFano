cab74e6378262a32e7ab085ae58e8dcb77cd986a
apdelsm <apdelsm@gmail.com>
2021-05-30 12:51:18 -0400

    fix byte size calculation of red black tree

--- src/predecessorStructures/redBlackTree.cpp
+++ src/predecessorStructures/redBlackTree.cpp
@@ -31,9 +31,10 @@ tuple<uint64_t, uint64_t, sd_vector<>>* RedBlackTree::getPredecessor(uint64_t va
 }
 
 uint64_t RedBlackTree::eefsize() {
+  //every node use 32 bytes in pointers + memory of key and value
   uint64_t returnSize = 0;
   returnSize += sizeof(map<uint64_t, tuple<uint64_t, uint64_t, sd_vector<>>*>);
-  returnSize += (sizeof(uint64_t) + sizeof(tuple<uint64_t, uint64_t, sd_vector<>>*)) * redBlackTree.size();
+  returnSize += (sizeof(uint64_t) + sizeof(tuple<uint64_t, uint64_t, sd_vector<>>*) + 32) * redBlackTree.size();
   return returnSize;
 }
 
