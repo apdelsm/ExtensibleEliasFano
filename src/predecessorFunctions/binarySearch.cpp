5cb96c9924aae44dcd6e09504645f2f0dfe64302
apdelsm <apdelsm@gmail.com>
2021-07-25 16:36:49 -0400

    adapt binary search to new structure

--- src/predecessorFunctions/binarySearch.cpp
+++ src/predecessorFunctions/binarySearch.cpp
@@ -9,25 +9,27 @@ using namespace sdsl;
 #ifndef BINARYSEARCH
 #define BINARYSEARCH
 
-tuple<uint32_t, uint32_t, sd_vector<>>* binarySearch(vector<tuple<uint32_t, uint32_t, sd_vector<>>*> &orderedVector, uint32_t element) {
+tuple<uint32_t, sd_vector<>>* binarySearch(vector<tuple<uint32_t, sd_vector<>>> &orderedVector, uint32_t element, uint32_t *block) {
   uint32_t low = 0;
   uint32_t high = orderedVector.size() - 1;
   uint32_t mid;
-  if (element < get<1>(*(orderedVector[0]))) {
+  if (element < get<0>(orderedVector[0])) {
     return NULL;
   }
   while(low != high && low < high - 1) {
     mid = (low + high + 1) / 2;
-    if (element < get<1>(*(orderedVector[mid]))) {
+    if (element < get<0>(orderedVector[mid])) {
       high = mid - 1;
     } else {
       low = mid;
     }
   }
-  if (element < get<1>(*(orderedVector[high]))) {
-    return orderedVector[low];
+  if (element < get<0>(orderedVector[high])) {
+    *block = low; 
+    return &(orderedVector[low]);
   } else {
-    return orderedVector[high];
+    *block = high;
+    return &(orderedVector[high]);
   }
 }
 
